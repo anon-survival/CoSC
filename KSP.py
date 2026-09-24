5295175e3beea3590097b8aa8c5d1c5ac09c4cc9
anon-survival <wjdgh4325@yonsei.ac.kr>
2026-09-24 23:40:14 +0900

    Update KSP.py

--- KSP.py
+++ KSP.py
@@ -81,25 +81,8 @@ def KSP(args):
     print("KS-cal:", ksp_result_test[3].item())
     print("KM-cal:", ksp_result_test[4].item())
     print("IBS:", ksp_result_test[5].item())
-    print("KS-sum:", ksp_result_test[6].item())
-    print("KS-var:", ksp_result_test[7].item())
     print("PSR:", ksp_result_test[8].item())
     
-    workbook = load_workbook(filename='./tmp_test.xlsx')
-    sheet = workbook.active
-    last_row = sheet.max_row
-    sheet.cell(row=last_row+1, column=1, value=ksp_result_test[0].item())
-    sheet.cell(row=last_row+1, column=2, value=ksp_result_test[1].item())
-    sheet.cell(row=last_row+1, column=3, value=ksp_result_test[2].item())
-    sheet.cell(row=last_row+1, column=4, value=ksp_result_test[3].item())
-    sheet.cell(row=last_row+1, column=5, value=ksp_result_test[4].item())
-    sheet.cell(row=last_row+1, column=6, value=ksp_result_test[5].item())
-    sheet.cell(row=last_row+1, column=7, value=ksp_result_test[6].item())
-    sheet.cell(row=last_row+1, column=8, value=ksp_result_test[7].item())
-    sheet.cell(row=last_row+1, column=9, value=ksp_result_test[8].item())
-    sheet.cell(row=last_row+1, column=10, value=(f'{args.dataset}_{args.model_dist}_ksp'))
-    workbook.save('./tmp_test.xlsx')
-
 if __name__ == '__main__':
     parser = TestArgParser()
     args = parser.parse_args()
